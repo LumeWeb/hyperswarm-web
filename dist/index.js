ce8edbdaa05d6108cf3e8b5be416f2ef618f1586
Derrick Hammer <derrick@derrickhammer.com>
2023-01-31 07:39:11 -0500

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,7 @@ export default class HyperswarmWeb extends EventEmitter {
         }
         const relays = this.relays;
         do {
-            const index = await randomNumber(0, relays.length - 1);
+            const index = relays.length > 1 ? await randomNumber(0, relays.length - 1) : 0;
             const relay = relays[index];
             let ret;
             try {
