35e394fec479314d61e8451fe8c28e97bb970604
Derrick Hammer <derrick@derrickhammer.com>
2023-01-31 05:10:12 -0500

    *rewrite for new design

--- src/index.ts
+++ src/index.ts
@@ -2,99 +2,80 @@
 import DhtNode from "@hyperswarm/dht-relay";
 // @ts-ignore
 import Stream from "@hyperswarm/dht-relay/ws";
-// @ts-ignore
-import createRoundRobin from "@derhuerst/round-robin-scheduler";
-// @ts-ignore
-// @ts-ignore
-import { blake2b, errTuple, hexToBuf } from "libskynet";
-// @ts-ignore
-import { registryRead } from "libkmodule";
+import { createClient } from "@lumeweb/kernel-peer-discovery-client";
+import type {
+  PeerDiscoveryClient,
+  Peer,
+} from "@lumeweb/kernel-peer-discovery-client";
+import { load } from "@lumeweb/libkernel-universal";
 
-import { unpack } from "msgpackr";
+// @ts-ignore
+import Hyperswarm from "hyperswarm";
 import randomNumber from "random-number-csprng";
+import EventEmitter from "node:events";
 
-const REGISTRY_DHT_KEY = "lumeweb-dht-node";
-
-export default class DHT {
+export default class HyperswarmWeb extends EventEmitter {
   private _options: any;
-  private _relays: Map<string, string> = new Map();
-  private _activeRelays: Map<string, typeof DhtNode> = new Map();
-  private _maxConnections = 10;
-  private _inited = false;
-
-  constructor(opts = {}) {
-    // @ts-ignore
+  private _relays: Set<string> = new Set();
+  private _activeRelay: Hyperswarm;
+  private _discovery: PeerDiscoveryClient;
+  constructor(opts: any = {}) {
+    super();
     opts.custodial = false;
     this._options = opts;
+    this._discovery = createClient();
   }
 
   ready(): Promise<void> {
-    if (this._inited) {
-      return Promise.resolve();
-    }
-
-    this._inited = true;
-    return this.fillConnections();
+    return this.ensureConnection();
   }
 
-  get relays(): string[] {
-    return [...this._relays.keys()];
-  }
+  private async ensureConnection(): Promise<any> {
+    const logErr = (await load()).logErr;
 
-  get relayServers(): string[] {
-    return [...this._relays.values()];
-  }
-
-  public async addRelay(pubkey: string): Promise<boolean> {
-    let entry: errTuple = await registryRead(
-      hexToBuf(pubkey).shift() as Uint8Array,
-      hashDataKey(REGISTRY_DHT_KEY)
-    );
-
-    if (entry[1] || !entry[0]?.exists) {
-      return false;
+    if (this._activeRelay) {
+      return;
     }
 
-    let host;
+    const relays = this.relays;
 
-    try {
-      host = unpack(entry[0].entryData);
-    } catch (e) {
-      return false;
+    do {
+      const index = await randomNumber(0, relays.length - 1);
+      const relay = relays[index];
+
+      let ret;
+      try {
+        ret = await this._discovery.discover(relay);
+      } catch (e) {
+        logErr(e);
+        relays.splice(index, 1);
+        continue;
+      }
+
+      ret = ret as Peer;
+
+      const connection = `wss://${ret.host}:${ret.port}`;
+
+      if (!(await this.isServerAvailable(connection))) {
+        relays.splice(index, 1);
+        continue;
+      }
+
+      this._activeRelay = new DhtNode(
+        new Stream(true, new WebSocket(connection)),
+        this._options
+      );
+
+      this._activeRelay.on("close", () => {
+        this._activeRelay = undefined;
+      });
+    } while (relays.length > 0);
+
+    if (!this._activeRelay) {
+      throw new Error("Failed to find an available relay");
     }
 
-    const [domain, port] = host.split(":");
-
-    if (isNaN(parseInt(port))) {
-      return false;
-    }
-
-    this._relays.set(pubkey, `wss://${domain}:${port}/`);
-
-    if (this._inited) {
-      await this.fillConnections();
-    }
-
-    return true;
-  }
-
-  public removeRelay(pubkey: string): boolean {
-    if (!this._relays.has(pubkey)) {
-      return false;
-    }
-
-    if (this._activeRelays.has(pubkey)) {
-      this._activeRelays.get(pubkey).destroy();
-      this._activeRelays.delete(pubkey);
-    }
-
-    this._relays.delete(pubkey);
-
-    return true;
-  }
-
-  public clearRelays(): void {
-    [...this._relays.keys()].forEach(this.removeRelay);
+    await this._activeRelay.dht.ready();
   }
 
   private async isServerAvailable(connection: string): Promise<boolean> {
@@ -109,99 +90,67 @@ export default class DHT {
       });
     });
   }
-
   async connect(pubkey: string, options = {}): Promise<DhtNode> {
-    if (this._activeRelays.size === 0) {
-      await this.fillConnections();
-    }
-    if (this._activeRelays.size === 0) {
-      throw new Error("Failed to find an available relay");
+    if (!this._activeRelay) {
+      await this.ensureConnection();
     }
 
-    let index = 0;
-
-    if (this._activeRelays.size > 1) {
-      index = await randomNumber(0, this._activeRelays.size - 1);
-    }
-
-    const node = this._activeRelays.get([...this._activeRelays.keys()][index]);
-
-    return node.connect(pubkey, options);
+    return this._activeRelay.connect(pubkey, options);
   }
 
-  private async fillConnections(): Promise<any> {
-    let available: string[] = [];
+  get relays(): string[] {
+    return [...this._relays.values()];
+  }
 
-    const updateAvailable = () => {
-      available = [...this._relays.keys()].filter(
-        (x) => ![...this._activeRelays.keys()].includes(x)
-      );
-    };
-    updateAvailable();
+  public async addRelay(pubkey: string): Promise<void> {
+    this._relays.add(pubkey);
+  }
 
-    let relayPromises = [];
-    while (
-      this._activeRelays.size <=
-      Math.min(this._maxConnections, available.length)
-    ) {
-      if (0 === available.length) {
-        break;
-      }
-
-      let relayIndex = 0;
-
-      if (available.length > 1) {
-        relayIndex = await randomNumber(0, available.length - 1);
-      }
-
-      const pubkey = available[relayIndex];
-      const connection = this._relays.get(available[relayIndex]) as string;
-
-      if (!(await this.isServerAvailable(connection))) {
-        available.splice(relayIndex, 1);
-        this.removeRelay(available[relayIndex]);
-        continue;
-      }
-
-      const node = new DhtNode(
-        new Stream(true, new WebSocket(connection)),
-        this._options
-      );
-      this._activeRelays.set(available[relayIndex], node);
-      updateAvailable();
-
-      relayPromises.push(node.ready());
-
-      node._protocol._stream.on("close", () => {
-        this._activeRelays.delete(pubkey);
-      });
+  public removeRelay(pubkey: string): boolean {
+    if (!this._relays.has(pubkey)) {
+      return false;
     }
 
-    return Promise.allSettled(relayPromises);
+    this._relays.delete(pubkey);
+
+    return true;
   }
-}
 
-export function hashDataKey(dataKey: string): Uint8Array {
-  return blake2b(encodeUtf8String(dataKey));
-}
-
-function encodeUtf8String(str: string): Uint8Array {
-  const byteArray = stringToUint8ArrayUtf8(str);
-  const encoded = new Uint8Array(8 + byteArray.length);
-  encoded.set(encodeNumber(byteArray.length));
-  encoded.set(byteArray, 8);
-  return encoded;
-}
-
-function stringToUint8ArrayUtf8(str: string): Uint8Array {
-  return new TextEncoder().encode(str);
-}
-
-function encodeNumber(num: number): Uint8Array {
-  const encoded = new Uint8Array(8);
-  for (let index = 0; index < encoded.length; index++) {
-    encoded[index] = num & 0xff;
-    num = num >> 8;
+  public clearRelays(): void {
+    this._relays.clear();
   }
-  return encoded;
+
+  on(
+    eventName: string | symbol,
+    listener: (...args: any[]) => void
+  ): Hyperswarm {
+    return this._activeRelay?.on(eventName, listener);
+  }
+  addListener(
+    eventName: string | symbol,
+    listener: (...args: any[]) => void
+  ): this {
+    return this.on(eventName, listener);
+  }
+
+  off(
+    eventName: string | symbol,
+    listener: (...args: any[]) => void
+  ): Hyperswarm {
+    return this._activeRelay?.off(eventName, listener);
+  }
+
+  removeListener(
+    eventName: string | symbol,
+    listener: (...args: any[]) => void
+  ): this {
+    return this.on(eventName, listener);
+  }
+  emit(eventName: string | symbol, ...args: any[]): boolean {
+    return this._activeRelay?.emit(eventName, ...args);
+  }
+
+  once(eventName: string | symbol, listener: (...args: any[]) => void): this {
+    return this._activeRelay?.once(eventName, listener);
+  }
 }
